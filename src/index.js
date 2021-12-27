56355d0e3871bf841a091bca58602e17262ecb59
Lukasz Smutek <lukaszsmutek@gmail.com>
2021-12-27 10:50:40 +0100

    Move App component to separate file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const App = () => (
-  <div className='wrapper'>
-    <h1 className='mainHeader'>Hello World</h1>
-    <h2 className='secondaryHeader'>Hello Eduweb</h2>
-  </div>
-)
 
 ReactDOM.render(
 <App/>,
