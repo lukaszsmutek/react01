3afea1241d745711702fd6d3b56d69d49b0892ad
Lukasz Smutek <lukaszsmutek@gmail.com>
2021-12-27 19:14:42 +0100

    Add prop types to ListItem

--- src/Components/ListWrapper/ListItem/ListItem.js
+++ src/Components/ListWrapper/ListItem/ListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ListItem.css';
+import PropTypes from 'prop-types';
 
 const ListItem = ({
     image,
@@ -20,5 +21,14 @@ const ListItem = ({
     </li>
 );
 
+ListItem.propTypes = {
+    image:PropTypes.string.isRequired,
+    name:PropTypes.string.isRequired,
+    description:PropTypes.string,
+    twitterLink:PropTypes.string.isRequired}
+
+ListItem.defaultProps = {
+    description:"One of the React creators"
+}
 
 export default ListItem;
