0d9ff741e1bb34356f3d82d878223f0ead39f098
Lukasz Smutek <lukaszsmutek@gmail.com>
2021-12-27 11:02:47 +0100

    Add assets and data folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React from 'react';
 
 const App = () => (
-  <div className='wrapper'>
-    <h1 className='mainHeader'>Hello World</h1>
-    <h2 className='secondaryHeader'>Hello Eduweb</h2>
-  </div>
-)
+    <div>
+        <h1>Hello World</h1>
+    </div>
+);
 
 export default App;
