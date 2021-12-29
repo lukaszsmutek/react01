afe1eb07fdc9226d83bc20ac3c1f96f13a2ccee1
Lukasz Smutek <lukaszsmutek@gmail.com>
2021-12-29 14:08:22 +0100

    Extraction of input component

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,53 +1,32 @@
 import React from 'react';
 import styles from "./Form.module.scss";
+import Input from '../Input/Input';
 
 const Form = ({submitFn}) => (
 <div className={styles.wrapper}>
         <h2>Add new twitter account</h2>
         <form 
             className={styles.form} onSubmit={submitFn}
+            autoComplete='off'
         >
-            <div className={styles.item}>
-                <input 
-                    type="text" 
-                    name="name" 
-                    id="name" placeholder=" " 
-                    required
-                    maxLength="30"
-                />
-                <label htmlFor="name">Name</label>
-                <div className={styles.item__bar}></div>
-            </div>
-            <div className={styles.item}>
-                <input
-                    type="text"
-                    name="link"
-                    required
-                    id="link" placeholder=" "
-                />
-                <label htmlFor="link">Link</label>
-                <div className={styles.item__bar}></div>
-            </div>
-            <div className={styles.item}>
-                <input
-                    type="text"
-                    name="image"
-                    id="image" 
-                    placeholder=" " 
-                />
-                <label htmlFor="image">Image</label>
-                <div className={styles.item__bar}></div>
-            </div>
-            <div className={styles.item}>
-                <textarea
-                    name="description"
-                    id="description"
-                    required
-                    placeholder=" "
-                />
-                <label htmlFor="description">Description</label>
-                <div className={styles.item__bar}></div>
-            </div>
+            <Input
+                name = "name"
+                label = "Name"
+                maxLength = {30}
+            />
+            <Input
+                name = "link"
+                label = "Twitter link"
+            />
+            <Input
+                name = "image"
+                label = "Image link"
+            />
+            <Input
+                tag = "textarea"
+                name = "description"
+                label = "Description"
+            />
             <button 
                 className={styles.button}
                 type="submit"
---
+++ src/Components/Input/Input.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './Input.module.scss';
+import PropTypes from 'prop-types';
+
+const Input = ({tag:Tag, name, label, maxLength}) => (
+
+    <div className={styles.item}>
+    <Tag
+        className={Tag==="textarea"?styles.textarea:styles.input}
+        type="text" 
+        name={name}
+        id={name}
+        required
+        maxLength={maxLength}
+        placeholder=" "
+    />
+    <label className = {styles.label} htmlFor={name}>{label}</label>
+    <div className={styles.item__bar}></div>
+</div>
+)
+
+Input.propTypes = {
+tag:PropTypes.string,
+name: PropTypes.string.isRequired,
+label:PropTypes.string.isRequired,
+maxLength: PropTypes.number,
+}
+
+Input.defaultProps = {
+    tag: 'input',
+    maxLength:200,
+}
+
+export default Input;
