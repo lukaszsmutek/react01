3c993c3cb3603e346b1c30490a49b87b8ab78697
Lukasz Smutek <lukaszsmutek@gmail.com>
2021-12-31 23:52:01 +0100

    Add styles to Header.js

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import styles from './Button.module.scss';
 
-const Button = ({children,href}) => (
+const Button = ({children,href, secondary}) => {
+    
+    const buttonClass = secondary ? styles.secondary : styles.button;
+    
+return (
     <>
         {
             href ? (
                 <a  href={href}
-                    className={styles.button}
+                    className={buttonClass}
                     rel = "noopener noreferrer"
                     target="_blank">
                     visit twitter page
                 </a>):(<button 
-        className={styles.button}
+        className={buttonClass}
         >
         {children}
         </button>)} 
     </>
 
-);
+)};
 
 
 export default Button;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import HeaderNavigation from './HeaderNavigation';
+import Button from '../Button/Button';
 import styles from './Header.module.scss';
+import Logo from '../../assets/images/logo.svg'
 
 const Header = () => (
-<div>
-    <HeaderNavigation className={styles}/>
-</div>
+<header className={styles.wrapper}>
+    <img src={Logo} alt ="Logo"/>
+    <HeaderNavigation/>
+    <Button secondary>new item</Button>
+</header>
 );
 
 export default Header;
